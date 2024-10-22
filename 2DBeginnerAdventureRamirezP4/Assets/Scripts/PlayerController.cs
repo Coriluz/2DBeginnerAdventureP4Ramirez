1f470dd71fc42d10601fe0f42d84dad175038df2
Coriluz <760351@student.dusd.net>
2024-10-22 11:43:08 -0700

    I created a script
    I'm now creating the tiles. Video stopped at 4:48

--- 2DBeginnerAdventureRamirezP4/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureRamirezP4/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0f;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        
+        
         transform.position = position;
     }
 }
